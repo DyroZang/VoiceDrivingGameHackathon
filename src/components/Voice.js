e2df882b987622aee2de06f9bf338f345b65dd59
Coda Bool <codabool@pm.me>
2022-10-16 12:30:32 -0400

    working game

--- src/components/Voice.js
+++ src/components/Voice.js
@@ -1,7 +1,11 @@
 import {useState, useEffect} from 'react'
+import { Row } from 'react-bootstrap';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import MicLiveGif from '../assets/mic-live.gif'
 
-export default function Voice() {
+export default function Voice({ state, testing, setCmd }) {
+  const [message, setMessage] = useState('')
+
   const {
     transcript,
     listening,
@@ -13,13 +17,49 @@ export default function Voice() {
     return <span>Browser doesn't support speech recognition. Please use Chrome</span>
   }
 
+  useEffect(() => {
+    if (!setCmd) return
+    const recentMsg = transcript.split(' ').slice(-1)[0].toLowerCase()
+    if (recentMsg.includes('left')) {
+      setCmd('left')
+    } else if (recentMsg.includes('right')) {
+      setCmd('right')
+    } else if (recentMsg.includes('up')) {
+      setCmd('up')
+    } else if (recentMsg.includes('down')) {
+      setCmd('down')
+    } else if (recentMsg.includes('stop')) {
+      setCmd('stop')
+    }
+  }, [transcript])
+
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true })
+
+    const intervalId = setInterval(() => {
+      resetTranscript()
+    }, 1000 * 60) // in milliseconds
+    return () => clearInterval(intervalId)
+  }, [])
+
+
   return (
     <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <Row>
+        {listening 
+          ? <img src={MicLiveGif} className="m-auto" style={{width: '110px', height: '80px'}} />
+          : <p className="text-center">Mic Not listening</p>
+        }
+      </Row>
+      {/* <button onClick={SpeechRecognition.startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <button onClick={resetTranscript}>Reset</button> */}
+      {transcript}
+      <hr />
+      {message}
+      {listening && !transcript
+        && <p className='mt-2'>try saying something into the mic</p>
+      }
     </div>
   )
 }
